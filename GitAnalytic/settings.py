ee4949d1f7a2c214bfbc43305aa1a887c779edda
lamha <lam@trip.vn>
2020-05-11 13:59:01 +0700

    INIT

--- GitAnalytic/settings.py
+++ GitAnalytic/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
